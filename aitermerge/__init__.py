f973bebc30821d542685fd2d3cd46e1afd974177
Fenhl <fenhl@fenhl.net>
2018-02-05 18:27:08 +0000

    Add the function

---
+++ aitermerge/__init__.py
@@ -0,0 +1,28 @@
+import aitertools
+import asyncio
+
+async def merge(aiters):
+    aiters = await aitertools.aiter(aiters)
+    iters = [None]
+    nexts = [asyncio.ensure_future(aitertools.anext(aiters))]
+    while len(iters) > 0:
+        await asyncio.wait(nexts, return_when=asyncio.FIRST_COMPLETED)
+        new_iters = []
+        completed_iters = set()
+        for i, future in enumerate(nexts):
+            if future.done():
+                try:
+                    if iters[i] is None:
+                        # new iterator
+                        new_iters.append(await aitertools.aiter(future.result()))
+                    else:
+                        # new item
+                        yield future.result()
+                    nexts[i] = asyncio.ensure_future(aitertools.anext(aiters if iters[i] is None else iters[i]))
+                except StopAsyncIteration:
+                    completed_iters.add(i)
+        for i in sorted(completed_iters, reverse=True):
+            iters.pop(i)
+            nexts.pop(i)
+        iters += new_iters
+        nexts += [asyncio.ensure_future(aitertools.anext(new_iter)) for new_iter in new_iters]
